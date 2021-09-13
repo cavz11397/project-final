a7310682e04dfc94ea582885f69f4014ac666c1f
Alejo <cavz11397@gmail.com>
2021-09-12 22:21:20 -0500

    [UPDATE] despliegue a la nube

--- src/helpers/getCard.js
+++ src/helpers/getCard.js
@@ -1,5 +1,5 @@
 const getCard = async (deck_id) => {
-    const url = `http://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`;
+    const url = `https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`;
     const res = await fetch(url);
     const data = await res.json();
     return data;
