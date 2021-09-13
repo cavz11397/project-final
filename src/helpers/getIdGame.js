accf87f1191a1c6e7b3347b97b8479260e6fd328
Alejo <cavz11397@gmail.com>
2021-09-12 22:17:06 -0500

    [update] actualizacion del metodo http

--- src/helpers/getIdGame.js
+++ src/helpers/getIdGame.js
@@ -1,5 +1,5 @@
 const getIdGame = async () => {
-    const url = `http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`;
+    const url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`;
     const res = await fetch(url,{ mode: 'cors', method: 'GET'});
     const data = await res.json();
     return data;
